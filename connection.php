bb5c8c9c40097706b5fc0a79ec2da4562e728b51
Parth Patel <patelp94@montclair.edu>
2019-03-21 01:14:30 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
 session_start();
  function connect(){
         $hostname = "Localhost";
-	    $username = "patelp94_parth";
-	    $password = "baseballsucks1";
-	    $dbname ="patelp94_testDB";
+	    $username = "Your Username";
+	    $password = "Your password";
+	    $dbname ="Your databaseNAME";
 	    
 	    static $link;
 	    $link = mysqli_connect($hostname, $username, $password, $dbname);
